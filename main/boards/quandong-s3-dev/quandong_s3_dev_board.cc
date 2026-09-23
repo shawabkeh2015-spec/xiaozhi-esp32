56348868f26a636a6162582f295e2812babd70bd
shawabkeh2015-spec <shawabkeh2015@gmail.com>
2026-09-22 21:55:34 -0500

    Add push-to-talk for Quandong S3

--- main/boards/quandong-s3-dev/quandong_s3_dev_board.cc
+++ main/boards/quandong-s3-dev/quandong_s3_dev_board.cc
@@ -59,110 +59,218 @@ private:
                 .enable_internal_pullup = 1,
             },
         };
-        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
+
+        ESP_ERROR_CHECK(
+            i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_)
+        );
     }
 
     void InitializeSpi() {
         spi_bus_config_t buscfg = {};
+
         buscfg.mosi_io_num = DISPLAY_SPI_MOSI_PIN;
         buscfg.miso_io_num = GPIO_NUM_NC;
         buscfg.sclk_io_num = DISPLAY_SPI_SCK_PIN;
         buscfg.quadwp_io_num = GPIO_NUM_NC;
         buscfg.quadhd_io_num = GPIO_NUM_NC;
-        buscfg.max_transfer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t);
-        ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
+        buscfg.max_transfer_sz =
+            DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t);
+
+        ESP_ERROR_CHECK(
+            spi_bus_initialize(
+                SPI2_HOST,
+                &buscfg,
+                SPI_DMA_CH_AUTO
+            )
+        );
     }
 
     // 板上音频功放使能引脚，需置为低电平
     void InitializeAudioPaEnable() {
         gpio_config_t io_conf = {};
-        io_conf.pin_bit_mask = (1ULL << AUDIO_PA_ENABLE_PIN);
+
+        io_conf.pin_bit_mask =
+            (1ULL << AUDIO_PA_ENABLE_PIN);
+
         io_conf.mode = GPIO_MODE_OUTPUT;
         io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
         io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
         io_conf.intr_type = GPIO_INTR_DISABLE;
+
         gpio_config(&io_conf);
-        gpio_set_level(AUDIO_PA_ENABLE_PIN, 0);
+
+        gpio_set_level(
+            AUDIO_PA_ENABLE_PIN,
+            0
+        );
     }
 
+    // Push-to-talk:
+    // Hold BOOT = listen
+    // Release BOOT = stop listening
     void InitializeButtons() {
-        // 与 bread-compact-wifi 一致：启动期间按下 BOOT 进入配网，否则切换对话状态
-        boot_button_.OnClick([this]() {
-            auto& app = Application::GetInstance();
-            if (app.GetDeviceState() == kDeviceStateStarting) {
-                EnterWifiConfigMode();
-                return;
-            }
-            app.ToggleChatState();
+
+        boot_button_.OnPressDown([this]() {
+            Application::GetInstance().StartListening();
+        });
+
+        boot_button_.OnPressUp([this]() {
+            Application::GetInstance().StopListening();
         });
     }
 
     void InitializeIli9341Display() {
+
         esp_lcd_panel_io_handle_t panel_io = nullptr;
         esp_lcd_panel_handle_t panel = nullptr;
 
         ESP_LOGD(TAG, "Install panel IO");
+
         esp_lcd_panel_io_spi_config_t io_config = {};
-        io_config.cs_gpio_num = DISPLAY_SPI_CS_PIN;
-        io_config.dc_gpio_num = DISPLAY_DC_PIN;
+
+        io_config.cs_gpio_num =
+            DISPLAY_SPI_CS_PIN;
+
+        io_config.dc_gpio_num =
+            DISPLAY_DC_PIN;
+
         io_config.spi_mode = 0;
-        io_config.pclk_hz = 40 * 1000 * 1000;
+
+        io_config.pclk_hz =
+            40 * 1000 * 1000;
+
         io_config.trans_queue_depth = 10;
         io_config.lcd_cmd_bits = 8;
         io_config.lcd_param_bits = 8;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI2_HOST, &io_config, &panel_io));
+
+        ESP_ERROR_CHECK(
+            esp_lcd_new_panel_io_spi(
+                SPI2_HOST,
+                &io_config,
+                &panel_io
+            )
+        );
 
         ESP_LOGD(TAG, "Install LCD driver");
+
         const ili9341_vendor_config_t vendor_config = {
-            .init_cmds = &vendor_specific_init[0],
-            .init_cmds_size = sizeof(vendor_specific_init) / sizeof(ili9341_lcd_init_cmd_t),
+            .init_cmds =
+                &vendor_specific_init[0],
+
+            .init_cmds_size =
+                sizeof(vendor_specific_init) /
+                sizeof(ili9341_lcd_init_cmd_t),
         };
 
         esp_lcd_panel_dev_config_t panel_config = {};
-        panel_config.reset_gpio_num = GPIO_NUM_NC;
+
+        panel_config.reset_gpio_num =
+            GPIO_NUM_NC;
+
         panel_config.flags.reset_active_high = 1;
-        panel_config.rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB;
+
+        panel_config.rgb_ele_order =
+            LCD_RGB_ELEMENT_ORDER_RGB;
+
         panel_config.bits_per_pixel = 16;
-        panel_config.vendor_config = (void *)&vendor_config;
-        ESP_ERROR_CHECK(esp_lcd_new_panel_ili9341(panel_io, &panel_config, &panel));
+
+        panel_config.vendor_config =
+            (void *)&vendor_config;
+
+        ESP_ERROR_CHECK(
+            esp_lcd_new_panel_ili9341(
+                panel_io,
+                &panel_config,
+                &panel
+            )
+        );
 
         esp_lcd_panel_reset(panel);
         esp_lcd_panel_init(panel);
-        esp_lcd_panel_invert_color(panel, true);
-        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
-        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
-        esp_lcd_panel_disp_on_off(panel, true);
 
-        display_ = new SpiLcdDisplay(panel_io, panel,
-            DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y,
-            DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
+        esp_lcd_panel_invert_color(
+            panel,
+            true
+        );
+
+        esp_lcd_panel_swap_xy(
+            panel,
+            DISPLAY_SWAP_XY
+        );
+
+        esp_lcd_panel_mirror(
+            panel,
+            DISPLAY_MIRROR_X,
+            DISPLAY_MIRROR_Y
+        );
+
+        esp_lcd_panel_disp_on_off(
+            panel,
+            true
+        );
+
+        display_ = new SpiLcdDisplay(
+            panel_io,
+            panel,
+            DISPLAY_WIDTH,
+            DISPLAY_HEIGHT,
+            DISPLAY_OFFSET_X,
+            DISPLAY_OFFSET_Y,
+            DISPLAY_MIRROR_X,
+            DISPLAY_MIRROR_Y,
+            DISPLAY_SWAP_XY
+        );
     }
 
 public:
-    QuandongS3DevBoard() : boot_button_(BOOT_BUTTON_GPIO) {
+
+    QuandongS3DevBoard()
+        : boot_button_(BOOT_BUTTON_GPIO) {
+
         InitializeI2c();
+
         InitializeSpi();
+
         InitializeAudioPaEnable();
+
         InitializeIli9341Display();
+
         InitializeButtons();
+
         GetBacklight()->SetBrightness(100);
     }
 
     virtual AudioCodec* GetAudioCodec() override {
+
         static Es8311AudioCodec audio_codec(
+
             codec_i2c_bus_,
+
             I2C_NUM_0,
+
             AUDIO_INPUT_SAMPLE_RATE,
+
             AUDIO_OUTPUT_SAMPLE_RATE,
+
             AUDIO_I2S_GPIO_MCLK,
+
             AUDIO_I2S_GPIO_BCLK,
+
             AUDIO_I2S_GPIO_WS,
+
             AUDIO_I2S_GPIO_DOUT,
+
             AUDIO_I2S_GPIO_DIN,
+
             AUDIO_CODEC_PA_PIN,
+
             AUDIO_CODEC_ES8311_ADDR,
+
             true,
-            true);
+
+            true
+        );
+
         return &audio_codec;
     }
 
@@ -171,7 +279,12 @@ public:
     }
 
     virtual Backlight* GetBacklight() override {
-        static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
+
+        static PwmBacklight backlight(
+            DISPLAY_BACKLIGHT_PIN,
+            DISPLAY_BACKLIGHT_OUTPUT_INVERT
+        );
+
         return &backlight;
     }
 };
